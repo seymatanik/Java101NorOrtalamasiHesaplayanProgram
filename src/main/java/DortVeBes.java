fb723ef59e66284d1283f7e5ce458a01301b4f19
SEYMA <seymanurrtanik@gmail.com>
2022-08-11 18:09:12 +0300

    DortVeBes.java

---
+++ src/main/java/DortVeBes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DortVeBes {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sinir Sayisini Giriniz:");
+        n = input.nextInt();
+
+        for (i = 1; i <= n; i *= 4) {
+            System.out.println(i);
+        }
+
+            for (int j = 1; j <= n; j*= 5) {
+                System.out.println(j);
+
+
+            }
+
+        }
+
+    }
